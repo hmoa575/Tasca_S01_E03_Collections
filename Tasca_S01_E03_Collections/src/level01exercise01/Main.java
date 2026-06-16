c9d4c841d212703398ed5154a540768c8d2b2adc
hmoa575 <hmoa575@outlook.com>
2026-06-16 10:53:29 +0200

    feat: add Month class, Main class

---
+++ Tasca_S01_E03_Collections/src/level01exercise01/Main.java
@@ -0,0 +1,56 @@
+package level01exercise01;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        List<Month> monthsList = new ArrayList<>();
+        monthsList.add(new Month("January"));
+        monthsList.add(new Month("February"));
+        monthsList.add(new Month("March"));
+        monthsList.add(new Month("April"));
+        monthsList.add(new Month("May"));
+        monthsList.add(new Month("June"));
+        monthsList.add(new Month("July"));
+        monthsList.add(new Month("September"));
+        monthsList.add(new Month("October"));
+        monthsList.add(new Month("November"));
+        monthsList.add(new Month("December"));
+
+        System.out.println("\n+++++ List without the August month.");
+        for(Month o : monthsList){
+            System.out.println(o.getName());
+        }
+
+        System.out.println("\n+++++ Adding August to the list.");
+        monthsList.add(7,new Month("August"));
+
+        System.out.println("\n+++++ List complete with August month.");
+        for(Month o : monthsList){
+            System.out.println(o.getName());
+        }
+
+        // monthsList is converted to monthsSet.
+        Set<Month> monthsSet = new HashSet<>(monthsList);
+        System.out.println("\n+++++ HashSet, and verify duplicates are not allowed.");
+        boolean isAdded = monthsSet.add(new Month("April"));
+        System.out.println("is April added two times? " + isAdded);
+        System.out.println("HashSet total size: " + monthsSet.size());
+        System.out.println("The year has 12 months, so it's working!");
+
+
+        System.out.println("\n+++++ Loop for-each");
+        for(Month o : monthsSet){
+            System.out.println(o.getName());
+        }
+
+        System.out.println("\n+++++ Iterator");
+        Iterator<Month> iterator = monthsSet.iterator();
+        while(iterator.hasNext())
+        {
+            Month month = iterator.next();
+            System.out.println(month.getName());
+        }
+    }
+
+}
---
+++ Tasca_S01_E03_Collections/src/level01exercise01/Month.java
@@ -0,0 +1,34 @@
+package level01exercise01;
+
+import java.util.Objects;
+
+public class Month {
+    //Attributes
+    String name;
+    //Constructor
+    public Month(String name) {
+        this.name = name;
+    }
+    //Getters and Setters
+    public String getName(){return name;}
+
+    @Override
+    public boolean equals(Object object){
+        if(this == object){return true;}
+        if(object == null || getClass() != object.getClass()){return false;}
+        Month month = (Month) object;
+        return Objects.equals(name,month.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Month{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
